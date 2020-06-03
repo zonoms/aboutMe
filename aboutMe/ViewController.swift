3ec52501e6409f84656b10e94a413a97796527b3
zon <zon_click@mail.ru>
2020-06-03 23:24:33 +0300

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by ADMIN on 03.06.2020.
+//  Copyright © 2020 Arkadij Ryabov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
